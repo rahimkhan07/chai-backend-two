d98ea942ff43a32cbefd8d1641b2ef2e8cbb7330
rahimkhan07 <140167765+rahimkhan07@users.noreply.github.com>
2025-07-05 01:53:07 +0530

    add mongoose

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+// import express from "express"
+
+// ( async () => {
+//     try{
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.error("ERROR: ", error)
+//         throw err
+//     }
+// })()
